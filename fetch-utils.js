b71430c50a7b1db86608888f758751b2db3aaec1
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-15 13:30:38 -0600

    adding fetch-utils.js and authorization

---
+++ fetch-utils.js
@@ -0,0 +1,39 @@
+// Enter Supabase Information
+const SUPABASE_URL = 'https://gmpyleofggphhfqygglb.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtcHlsZW9mZ2dwaGhmcXlnZ2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NTEsImV4cCI6MTk1OTkxNzQ1MX0.aAoG-W_B2pk78Kdb54K8sM3SQbO0g1kbGUOtqvvQhXA';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+
+    return response.user;
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
+
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('/');
+}
+
+export async function redirectIfLoggedIn() {
+    if (await getUser()) {
+        location.replace('./redirect');
+    }
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '../';
+}
