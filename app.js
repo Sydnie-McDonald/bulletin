286abd22ba21fcb97d08f397f00a76df2cd353ac
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-15 19:33:45 -0600

    working logged in and made a post then logged out .

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
+import { redirectIfLoggedIn, signupUser } from './fetch-utils.js';
+// grab my DOM elements and where I am updating to the html
+const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+redirectIfLoggedIn();
 
-// let state
+signUpForm.addEventListener('submit', async (e) => {
+    // prevents the default action when a link is clicked, page refresh 
+    e.preventDefault();
+    // grab the email value and password value
+    await signupUser(signUpEmail.value, signUpPassword.value);
+    // run redirect if logged in to make sure the user is verified and log them in redirect to other page
+    redirectIfLoggedIn();
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+});
